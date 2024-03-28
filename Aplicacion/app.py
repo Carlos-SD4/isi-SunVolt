d1cb39eb3737bc9c4f06fc6cdefd6c17387771b7
javier-pardo4 <javier.pardo4@alu.uclm.es>
2024-03-28 19:13:12 +0100

    InicioSesion

--- Aplicacion/app.py
+++ Aplicacion/app.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template
 from APIs import NREL, OpenWeatherMap, REData
 
-
 app = Flask(__name__)
 
-@app.route('/pagina-principal')
+@app.route('/InicioSesion')
 def pagina_principal():
-    # Llama al método mostrar_datos_en_espanol del módulo NREL
-    NREL.mostrar_datos_en_espanol()
     OpenWeatherMap.obtener_tiempo('Madrid')
     REData.get_real_time_market_prices()
 
-    # Resto de tu código aquí...
+    # Renderiza la plantilla InicioSesion.html
+    return render_template('InicioSesion.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
