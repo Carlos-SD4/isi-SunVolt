e8b4175f6051265c127de5e2eb7f0eacb67efbbc
Carlos-SD4 <146247295+Carlos-SD4@users.noreply.github.com>
2024-03-28 20:01:32 +0100

    Update app.py

--- Aplicacion/app.py
+++ Aplicacion/app.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template
-from APIs import NREL, OpenWeatherMap, REData
-
+from flask import Flask, render_template, jsonify, request
+from OpenWeatherMap import obtener_tiempo  # Importar la función obtener_tiempo
 
 app = Flask(__name__)
 
-@app.route('/pagina-principal')
-def pagina_principal():
-    # Llama al método mostrar_datos_en_espanol del módulo NREL
-    NREL.mostrar_datos_en_espanol()
-    OpenWeatherMap.obtener_tiempo('Madrid')
-    REData.get_real_time_market_prices()
+@app.route('/')
+def index():
+    return render_template('InicioSesion.html')
 
-    # Resto de tu código aquí...
+@app.route('/tiempo')
+def tiempo():
+    provincia = request.args.get('provincia')  # Obtener la provincia desde la solicitud
+
+    predicciones = obtener_tiempo(provincia)  # Usar la función obtener_tiempo
+
+    return render_template('PaginaPrincipal.html', predicciones=predicciones)  # Renderizar Estadisticas.html con los datos de predicciones meteorológicas
 
 if __name__ == '__main__':
     app.run(debug=True)
