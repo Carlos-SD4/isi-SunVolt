e113075bc5456a56dcb63fc147027381bc84468b
Carlos-SD4 <146247295+Carlos-SD4@users.noreply.github.com>
2024-03-28 20:00:22 +0100

    Update scripst.js

---
+++ Aplicacion/static/scripts/scripst.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Función para obtener los datos del tiempo
+    function obtenerTiempo(provincia) {
+        fetch(`/tiempo?provincia=${provincia}`)
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('temperature').textContent = data.temperatura + '°C';
+                document.getElementById('humidity').textContent = data.humedad + '%';
+                document.getElementById('cloudiness').textContent = data.nubosidad + '%';
+                document.getElementById('rain-probability').textContent = data.probabilidad_lluvia + '%';
+                document.getElementById('solar-radiation').textContent = data.radiacion_solar + ' W/m^2';
+            })
+            .catch(error => console.error('Error al obtener los datos:', error));
+    }
+
+    obtenerTiempo('Ciudad Real');
+});
