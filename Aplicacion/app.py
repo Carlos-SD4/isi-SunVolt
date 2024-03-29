a44a490a71ec04557551e0625092fc9028fca4d6
Carlos-SD4 <146247295+Carlos-SD4@users.noreply.github.com>
2024-03-29 12:58:38 +0100

    Update app.py

--- Aplicacion/app.py
+++ Aplicacion/app.py
@@ -1,19 +1,30 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template,request
 from OpenWeatherMap import obtener_tiempo  # Importar la función obtener_tiempo
+from REData import get_real_time_market_prices  # Importar la función get_real_time_market_prices
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/Principal')
 def index():
-    return render_template('InicioSesion.html')
-
-@app.route('/tiempo')
-def tiempo():
     provincia = request.args.get('provincia')  # Obtener la provincia desde la solicitud
 
     predicciones = obtener_tiempo(provincia)  # Usar la función obtener_tiempo
 
     return render_template('PaginaPrincipal.html', predicciones=predicciones)  # Renderizar Estadisticas.html con los datos de predicciones meteorológicas
+
+
+
+@app.route('/mis_dispositivos')
+def mis_dispositivos():
+    return render_template('Dispositivos.html')
+
+@app.route('/recomendaciones')
+def recomendaciones():
+    provincia = request.args.get('provincia', 'Madrid')  #Falta Ajustar tiempo y provincia con las apis
+    predicciones_tiempo = obtener_tiempo(provincia)
+    precioluz = get_real_time_market_prices()
+    return render_template('Recomendaciones.html', predicciones_tiempo=predicciones_tiempo, precioluz=precioluz)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
