68207cef2f20b0d843c42950e73234fa13e8d993
javier-pardo4 <javier.pardo4@alu.uclm.es>
2024-03-30 19:27:51 +0100

    Registro

--- Aplicacion/app.py
+++ Aplicacion/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
-from APIs import NREL, OpenWeatherMap, REData
 
 app = Flask(__name__)
 
 @app.route('/InicioSesion')
 def pagina_principal():
-    OpenWeatherMap.obtener_tiempo('Madrid')
-    REData.get_real_time_market_prices()
-
     # Renderiza la plantilla InicioSesion.html
     return render_template('InicioSesion.html')
 
+@app.route('/Registro')  # Agrega una nueva ruta para la página de registro
+def registro():
+    # Renderiza la plantilla Registro.html
+    return render_template('Registro.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
